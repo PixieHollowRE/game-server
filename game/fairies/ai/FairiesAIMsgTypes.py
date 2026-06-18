4628e74af5e27766f8469dd655b6462ea5cfd4c3
Jaden <me@rocketprogrammer.me>
2026-06-17 21:31:15 -0500

    comment this out
    disney...

--- game/fairies/ai/FairiesAIMsgTypes.py
+++ game/fairies/ai/FairiesAIMsgTypes.py
@@ -1,8 +1,8 @@
 from game.otp.ai.AIMsgTypes import *
 FairiesAIMsgName2Id = {}
 FairiesAIMsgId2Names = invertDictLossless(FairiesAIMsgName2Id)
-for name, value in list(FairiesAIMsgName2Id.items()):
-    exec('%s = %s' % (name, value))
+# for name, value in list(FairiesAIMsgName2Id.items()):
+    # exec('%s = %s' % (name, value))
 
-del name
-del value
+# del name
+# del value
