f4fe869730c4135c47a5a7f3c37e0072156051ac
Jaden <me@rocketprogrammer.me>
2026-09-20 09:52:08 -0500

    better

--- startup/unix/start_all.sh
+++ startup/unix/start_all.sh
@@ -2,5 +2,5 @@
 screen -dmS OTP ./start_otp_server.sh
 
 cd ../..
-screen -dmS UberDOG python3 -m game.fairies.uberdog.Start config/config.prc
+screen -dmS UberDOG ./start_uberdog.sh
 screen -dmS Districts python3 -m DistrictStarter
